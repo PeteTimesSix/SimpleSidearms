1c333c6c2a8e9b9e4d9f0180dcb1f3ca41f0d7d6
PeteTimesSix <Pete1001@seznam.cz>
2023-05-23 19:39:02 +0200

    Squashed commit of the following:
    commit aea09ddfa06464f34dc126af7f7dcdca8b61f3dc
    Author: PeteTimesSix <Pete1001@seznam.cz>
    Date:   Tue May 23 17:39:40 2023 +0200
    
        add missing patch class

--- Source/rimworld/CompSidearmMemory.cs
+++ Source/rimworld/CompSidearmMemory.cs
@@ -212,6 +212,8 @@ namespace SimpleSidearms.rimworld
                 return;
             }
             if (_cache == null) _cache = new Dictionary<int, CompSidearmMemory>();
+            if (_cache.ContainsKey(Owner.thingIDNumber) && _cache[Owner.thingIDNumber] != this)
+                Log.Warning($"SS: Pawn {Owner} had snuck in their memory comp from the previous game!");
             _cache[Owner.thingIDNumber] = this;
         }
 
